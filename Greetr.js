7254fea43476fe217b77d73febc040e93b81fb86
Matt <mknut001@gmail.com>
2016-07-25 11:46:20 -0700

    fix errors in Greetr.js

--- Greetr.js
+++ Greetr.js
@@ -2,20 +2,25 @@
 (function(global, $) {
 
   var Greetr = function(firstName, lastName, language) {
-    return new Greetr.init(firstname, lastName, language);
+    return new Greetr.init(firstName, lastName, language);
   }
 
   Greetr.prototype = {};
-  
-  Greetr.init = function(fistName, lastName, language) {
+
+  Greetr.init = function(firstName, lastName, language) {
 
     //function constructor to build the object and give it three properties //
     //sets the value if you pass it in, otherwise sets defaults//
     var self = this;
     self.firstName = firstName || '';
     self.lastName = lastName || '';
-    Self.language = language || 'en';
+    self.language = language || 'en';
 
   }
 
-}(window, jquery));
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
+
+
+}(window, jQuery));
