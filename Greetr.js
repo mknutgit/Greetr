9412e3e4237e062b5073f5da8fd93ba72aeca175
Matt <mknut001@gmail.com>
2016-07-25 12:51:17 -0700

    add jQuery method for greeting

--- Greetr.js
+++ Greetr.js
@@ -5,7 +5,106 @@
     return new Greetr.init(firstName, lastName, language);
   }
 
-  Greetr.prototype = {};
+  var supportedLangs = ['en', 'es'];
+
+  var greetings = {
+    en: 'helllo',
+    es: 'hola'
+  };
+
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+
+  Greetr.prototype = {
+
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function() {
+      if (supportedLangs.indexOf(this.language)  === -1) {
+        throw "Invalid language";
+      }
+    },
+
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+
+    greet: function(formal) {
+      var msg;
+
+      // if undefined or null it will be cerced to 'false'
+      if (formal) {
+        msg = this.formalGreeting();
+      }
+      else {
+        msg = this.greeting();
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+
+      // 'this' refers to the calling object at execution time
+      // makes the method chainable
+      return this;
+    },
+
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+
+      return this;
+    },
+
+    setLang: function(lang) {
+      this.language = lang;
+
+      this.validate();
+
+      return this;
+    }
+
+    //jQuery method that accepts a jQuery selector and wether or not it is a formal greeting
+    //sets up the greeting and updates whatever value it there
+    HTMLGreeting: function(selector, formal) {
+      if (!$) {
+        throw 'jQuery not loaded';
+      }
+
+      if (!selector) {
+        throw 'Missing JQuery selector';
+      }
+
+      var msg;
+      if (formal) {
+        msg = this.formalGreeting();
+      }
+      else {
+        msg = this.greeting();
+      }
+
+      $(selector).html(msg);
+
+      return this;
+
+    }
+
+  };
 
   Greetr.init = function(firstName, lastName, language) {
 
